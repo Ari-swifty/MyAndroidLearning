e25843acf445acb706521511009c98bce85efae0
Ari-swifty <suthanswifty@gmail.com>
2023-04-14 23:15:31 +0530

    ViewModel+LiveData Learning Examples.

--- ViewModelExample/app/src/main/java/com/arigarasuthan/viewmodelexampleapp/MainActivity.kt
+++ ViewModelExample/app/src/main/java/com/arigarasuthan/viewmodelexampleapp/MainActivity.kt
@@ -13,16 +13,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var viewModelFactory: MainActivityViewModelFactory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModelFactory = MainActivityViewModelFactory(100)
-        viewModel = ViewModelProvider(this,viewModelFactory)[MainActivityViewModel::class.java]
-        viewModel.countTotalData.observe(this, Observer {
-            binding.countText.text = "$it"
-        })
-        binding.apply {
-             countButton.setOnClickListener {
-                 viewModel.getUpdatedCount()
-            }
-        }
+        viewModel = ViewModelProvider(this, viewModelFactory)[MainActivityViewModel::class.java]
+        binding.lifecycleOwner = this
+        binding.myviewModel = viewModel
     }
 }
