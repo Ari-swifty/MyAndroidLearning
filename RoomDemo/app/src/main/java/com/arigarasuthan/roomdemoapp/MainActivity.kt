92e180ee46579e1eeedee30a6df7ba779c381611
Ari-swifty <suthanswifty@gmail.com>
2023-04-30 19:38:50 +0530

    RoomDemo-click added

--- RoomDemo/app/src/main/java/com/arigarasuthan/roomdemoapp/MainActivity.kt
+++ RoomDemo/app/src/main/java/com/arigarasuthan/roomdemoapp/MainActivity.kt
@@ -3,11 +3,13 @@ package com.arigarasuthan.roomdemoapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.arigarasuthan.roomdemoapp.databinding.ActivityMainBinding
+import com.arigarasuthan.roomdemoapp.db.Subscriber
 import com.arigarasuthan.roomdemoapp.db.SubscriberDataBase
 
 class MainActivity : AppCompatActivity() {
@@ -15,27 +17,31 @@ class MainActivity : AppCompatActivity() {
     private lateinit var subscriberViewModel: SubscriberViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding  = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val dao = SubscriberDataBase.getInstance(this).subscriberDao
         val repo = SubscriberRepository(dao)
         val factory = SubscriberViewModelFactory(repo)
-        subscriberViewModel = ViewModelProvider(this,factory)[SubscriberViewModel::class.java]
+        subscriberViewModel = ViewModelProvider(this, factory)[SubscriberViewModel::class.java]
         binding.myViewModel = subscriberViewModel
         binding.lifecycleOwner = this
         initRecyclerView()
     }
 
-    private fun initRecyclerView()
-    {
+    private fun initRecyclerView() {
         binding.subscriberList.layoutManager = LinearLayoutManager(this)
         displaySubscriberList()
     }
 
-    private fun displaySubscriberList()
-    {
+    private fun displaySubscriberList() {
         subscriberViewModel.subscribers.observe(this) { subscribersList ->
-            Log.d("MyTag",subscribersList.toString())
-            binding.subscriberList.adapter = MyRecyclerViewAdapter(subscribersList)
+            Log.d("MyTag", subscribersList.toString())
+            binding.subscriberList.adapter = MyRecyclerViewAdapter(
+                subscribersList
+            ) { selectedItem: Subscriber -> listItemClicked(selectedItem) }
         }
+    }
+
+    private fun listItemClicked(subscriber: Subscriber) {
+        Toast.makeText(this, "Selected Name Is ${subscriber.name}", Toast.LENGTH_LONG).show()
     }
 }
--- RoomDemo/app/src/main/java/com/arigarasuthan/roomdemoapp/MyRecyclerViewAdapter.kt
+++ RoomDemo/app/src/main/java/com/arigarasuthan/roomdemoapp/MyRecyclerViewAdapter.kt
@@ -7,7 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.arigarasuthan.roomdemoapp.databinding.ListItemBinding
 import com.arigarasuthan.roomdemoapp.db.Subscriber
 
-class MyRecyclerViewAdapter(private val subscribers: List<Subscriber>) : RecyclerView.Adapter<MyViewHolder>(){
+class MyRecyclerViewAdapter(private val subscribers: List<Subscriber>,private val clickListener:(Subscriber)->Unit) : RecyclerView.Adapter<MyViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding:ListItemBinding = DataBindingUtil.inflate(layoutInflater,R.layout.list_item,parent,false)
@@ -17,13 +17,16 @@ class MyRecyclerViewAdapter(private val subscribers: List<Subscriber>) : Recycle
     override fun getItemCount(): Int = subscribers.size
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.bind(subscribers[position])
+        holder.bind(subscribers[position],clickListener)
     }
 }
 class MyViewHolder(val binding:ListItemBinding) : RecyclerView.ViewHolder(binding.root)
 {
-    fun bind(subscriber: Subscriber) {
+    fun bind(subscriber: Subscriber,clickListener:(Subscriber)->Unit) {
         binding.nameText.text = subscriber.name
         binding.emailText.text = subscriber.email
+        binding.listItemLayout.setOnClickListener {
+            clickListener(subscriber)
+        }
     }
 }
