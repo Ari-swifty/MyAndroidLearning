69d87f79dd41d5da95f4cafc978f7684bb028f36
Ari-swifty <suthanswifty@gmail.com>
2023-05-11 12:11:13 +0530

    Hilt Demo App

---
+++ HiltDemoProject/app/src/main/java/com/arigarasuthan/hiltdemoapp/DataModule.kt
@@ -0,0 +1,14 @@
+package com.arigarasuthan.hiltdemoapp
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+class DataModule {
+
+    @Provides
+    fun provideDataSource():DataSource = DataSource()
+}
---
+++ HiltDemoProject/app/src/main/java/com/arigarasuthan/hiltdemoapp/DataSource.kt
@@ -0,0 +1,9 @@
+package com.arigarasuthan.hiltdemoapp
+
+import android.util.Log
+
+class DataSource {
+    fun getRemoteData() {
+        Log.i("MYTAG","Data Downloading..")
+    }
+}
